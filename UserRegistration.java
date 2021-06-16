fe274101f91085242ec2b71f50a50d368029c7c6
HajarePratik <pratikhajare09@gmail.com>
2021-06-16 19:29:21 +0530

    Validate User First Name

---
+++ UserRegistration.java
@@ -0,0 +1,36 @@
+package com.bridgelabz.user_registration_programs;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+
+	
+	// Method to validate the firstname
+	public boolean isvalidfirstname(String firstname) 
+	{
+		Pattern p;
+		Matcher m;
+		//Regex to check valid firstname
+		String  regex = "^[A-Z]{1}[a-z]{2,}";
+		p = Pattern.compile(regex);
+		m = p.matcher(firstname);
+		return m.matches();
+	}
+	
+	
+	public static void main(String[] args) 
+	{
+		
+		UserRegistration user = new UserRegistration();
+		Scanner sc = new Scanner(System.in);
+	
+		System.out.println("Enter a First Name :");
+		String firstname = sc.nextLine();
+		
+		System.out.println("Given First Name is :"+user.isvalidfirstname(firstname));
+		
+	}
+
+}
